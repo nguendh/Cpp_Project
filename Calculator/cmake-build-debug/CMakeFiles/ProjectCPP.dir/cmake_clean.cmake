f6ee54319c2be1814da96f87df8212ed0dfe8eba
nguendh <73826903+nguendh@users.noreply.github.com>
2021-12-05 21:16:05 +0300

    Add files via upload

---
+++ Calculator/cmake-build-debug/CMakeFiles/ProjectCPP.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectCPP.dir/BigInt.cpp.obj"
+  "CMakeFiles/ProjectCPP.dir/main.cpp.obj"
+  "ProjectCPP.exe"
+  "ProjectCPP.exe.manifest"
+  "ProjectCPP.pdb"
+  "libProjectCPP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
